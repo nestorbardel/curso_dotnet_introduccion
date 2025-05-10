01d3f1b261b41c9bef0f442bff9a4a06caf89b24
Nestor Bardel <nestorbardel@gmail.com>
2025-05-10 01:12:32 -0300

    Add initial .NET 9.0 projects for Variables and TipoDeDatos
    Initial commit includes setup for .NET 9.0 projects "Variables" and "TipoDeDatos". Added build files, project configurations, and example code for basic types demonstration in TipoDeDatos.

---
+++ introduccion/Variables/TipoDeDatos/Program.cs
@@ -0,0 +1,8 @@
+﻿int number = 12121;
+double doubleNumber = 12.1212;
+char character = 'A';
+string name = "Nestor The Best";
+bool isDead = false;
+
+Console.WriteLine(isDead);
+
---
+++ introduccion/Variables/Variables/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int number1 = 1;
+int number2 = 2;
+
+Console.WriteLine(number1);
+Console.WriteLine(number2);
+
+number1 = 3;
+
+Console.WriteLine(number1);
+
+int number3 = number1 + number2;
+
+Console.WriteLine(number3);
