df25a1f4e283813a948cc5c0899187d9a439013c
Nestor Bardel <nestorbardel@gmail.com>
2025-06-08 12:16:11 -0300

    arreglos

--- introduccion/Variables/Arreglos/Program.cs
+++ introduccion/Variables/Arreglos/Program.cs
@@ -1,3 +1,21 @@
-﻿string[] friends = new string[7];
+﻿string[] friends = new string[7]{
+    "Pancho",
+    "Paco",
+    "Ana",
+    "Ruben",
+    "Karla",
+    "Luis",
+    null
+};
 friends[0] = "Hector";
 System.Console.WriteLine(friends[0]);
+System.Console.WriteLine(friends[1]);
+System.Console.WriteLine(friends[2]);
+System.Console.WriteLine(friends[3]);
+System.Console.WriteLine(friends[4]);
+System.Console.WriteLine(friends[5]);
+System.Console.WriteLine(friends[6]);
+
+friends[6] = "Carlos";
+System.Console.WriteLine(friends[6]);
+
