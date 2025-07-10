36374bcc7cb839f74a5b9d0449656820566f81dd
Nestor <nestorbardel@gmail.com>
2025-07-10 01:06:26 -0300

    feat: Agregar proyectos Json y Static
    - Se agrega un nuevo proyecto para demostrar la serialización JSON.
    - Se agrega un nuevo proyecto para demostrar el uso de la palabra clave static.
    - Se actualiza el archivo de la solución para incluir los nuevos proyectos.

---
+++ introduccion/Variables/Json/Program.cs
@@ -0,0 +1,17 @@
+﻿Beer myBeer = new Beer()
+{
+    Name = "Pikantus",
+    Brand = "Erdinger"
+};
+
+Beer[] myBeers = new Beer[] 
+{ 
+    new Beer() { Name = "Pikantus", Brand = "Erdinger" },
+    new Beer() { Name = "Tuborg", Brand = "Tuborg" }
+};
+
+public class Beer
+{
+    public string Name { get; set; }
+    public string Brand { get; set; }
+}
---
+++ introduccion/Variables/Static/Program.cs
@@ -0,0 +1,42 @@
+﻿People nestor = new People()
+{
+    Name = "Nestor",
+    Age = 38
+};
+
+People juan = new People()
+{
+    Name = "Juan",
+    Age = 38
+};
+
+System.Console.WriteLine(People.GetCount());
+
+public static class A
+{
+    public static void Some()
+    {
+        Console.WriteLine("A");
+
+    }
+}
+
+
+public class People
+{
+    public static int Count = 0;
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public People()
+    {
+        Count++;
+    }
+
+    public static string GetCount()
+    {
+        return $"Esta clase se ha usado {Count} veces";
+    }
+
+
+}
