ca353a092fb3132213b70f12acf783815dc60a8f
Nestor Bardel <nestorbardel@gmail.com>
2025-07-11 19:02:19 -0300

    feat: Agregar proyecto "ExcepcionesPersonalizadas" con manejo de excepciones en C#

---
+++ introduccion/Variables/ExcepcionesPersonalizadas/Program.cs
@@ -0,0 +1,39 @@
+﻿try
+{
+    var beer = new Beer()
+    {
+        Name = "London Porter",
+        // Brand = "Fullers"
+    };
+
+    Console.WriteLine(beer);
+}
+catch (InvalidBeerException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+public class InvalidBeerException : Exception
+{
+    public InvalidBeerException() : base("La cerveza no tiene nombre o marca, por lo cual es invalida")
+    {
+        
+    }
+}
+
+
+
+public class Beer
+{
+    public string Name { get; set; }
+    public string Brand { get; set; }
+
+    public override string ToString()
+    {
+        if (this.Name == null || this.Brand == null)
+        {
+            throw new InvalidBeerException();
+        }
+        return $"Cerveza: {Name}, Brand: {Brand}";
+    }
+}
