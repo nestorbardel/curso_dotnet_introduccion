02bb37c4628682a8a35f6497664660f3ff762593
Nestor <nestorbardel@gmail.com>
2025-07-11 18:42:29 -0300

    feat: Agregar proyecto "Excepciones" con manejo de errores y ejemplos de excepciones en C#

---
+++ introduccion/Variables/Excepciones/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+try
+{
+    string content = File.ReadAllText(@"/Users/nestorbardel/Documents/prueba.rtf");
+    System.Console.WriteLine(content);
+
+    // string content2 = File.ReadAllText(@"/Users/nestorbardel/Documents/prueba2.rtf");
+    // System.Console.WriteLine(content2);
+
+    throw new Exception("Ocurrio algo inesperado");
+
+}
+catch (FileNotFoundException ex)
+{
+    Console.WriteLine($"El archivo no existe");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"{ex.Message}");
+}
+finally
+{
+    Console.WriteLine($"Aqui me he ejecutado pase lo que pase");
+    
+}
+
+Console.WriteLine($"Aqui se sigue ejecutando");
+
+
