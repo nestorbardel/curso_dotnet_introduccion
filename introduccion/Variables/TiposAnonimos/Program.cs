f16deb552d727014c25b1f12c31f8e9a02ce545d
Nestor <nestorbardel@gmail.com>
2025-07-11 15:35:15 -0300

    feat: Add "TiposAnonimos" project with examples of anonymous types
    - Created a new project "TiposAnonimos" to demonstrate the use of anonymous types in .NET.
    - Added examples using anonymous objects and collections.
    - Updated solution file to include the new project.

---
+++ introduccion/Variables/TiposAnonimos/Program.cs
@@ -0,0 +1,18 @@
+﻿var nestor = new
+{
+    Name = "Nestor",
+    Age = 38
+};
+
+Console.WriteLine($"{nestor.Name} {nestor.Age}");
+
+var beers = new[]
+{
+    new{Name = "Red", Brand="Delirium"},
+    new{Name = "London Porter", Brand="Fullers"}
+};
+
+foreach (var b in beers)
+{
+    Console.WriteLine($"{b.Name} {b.Brand}");
+}
