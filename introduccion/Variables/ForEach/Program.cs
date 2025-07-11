d44b6e20b34fc1e04321a2718b073f16a19e3906
Nestor <nestorbardel@gmail.com>
2025-07-11 10:14:59 -0300

    feat: Add example demonstrating List usage and custom class in "ForEach" project
    - Implemented `People` class to store name and country attributes.
    - Added a collection of `People` and demonstrated list operations such as addition and removal.
    - Included `Show` method to display the list contents with a formatted structure.

--- introduccion/Variables/ForEach/Program.cs
+++ introduccion/Variables/ForEach/Program.cs
@@ -8,3 +8,30 @@ foreach (var number in numbers)
     Console.WriteLine(number);
 }
 
+var students = new List<People>()
+{
+    new People() {Name="Hector", Country="Mexico"},
+    new People() {Name = "Roberto", Country = "Argentina"},
+    new People() {Name = "Ana", Country = "Estados Unidos"}
+};
+
+Show(students);
+
+students.RemoveAt(0);
+Show(students);
+
+static void Show(List<People> students)
+{
+    Console.WriteLine("----Personas----");
+    foreach (var people in students)
+    {
+        Console.WriteLine($"Nombre: {people.Name}, Pais: {people.Country}");
+    }
+}
+
+public class People
+{
+    public string Name { get; set; }
+    public string Country { get; set; }
+}
+
