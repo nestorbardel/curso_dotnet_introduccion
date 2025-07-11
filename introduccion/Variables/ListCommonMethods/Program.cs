ed3b721c215ad42aae68969ca4c79efc794ac282
Nestor Bardel <nestorbardel@gmail.com>
2025-07-11 11:15:28 -0300

    Add `ListCommonMethods` project with examples of `List` operations
    - Created `ListCommonMethods` project with examples showcasing various `System.Collections.Generic.List` methods, including `Insert`, `AddRange`, `Sort`, `Contains`, and `IndexOf`.
    - Added `ListCommonMethods` to `CsharpCourse.sln` and updated solution build configurations.

---
+++ introduccion/Variables/ListCommonMethods/Program.cs
@@ -0,0 +1,44 @@
+﻿var numbers = new List<int>()
+{
+    4,3,5,19
+};
+
+Show(numbers);
+
+numbers.Insert(1,6);
+
+Show(numbers);
+
+if (numbers.Contains(19))
+{
+    Console.WriteLine("Existe");
+}
+else
+{
+    Console.WriteLine("No existe");
+}
+
+int pos = numbers.IndexOf(19);
+Console.WriteLine($"Posicion: {pos}");
+
+numbers.Sort();
+Show(numbers);
+
+var numbers2 = new List<int>()
+{
+    200,300,400
+    
+};
+
+numbers.AddRange(numbers2);
+
+Show(numbers);
+
+static void Show(List<int> numbers)
+{
+    Console.WriteLine("----Numeros----");
+    foreach (var number in numbers)
+    {
+        Console.WriteLine(number);
+    }
+}
