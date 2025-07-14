6b83ddddd0595805f62cc0725fbfbd5f4da9854a
Nestor Bardel <nestorbardel@gmail.com>
2025-07-13 22:43:15 -0300

    feat: Add "LINQ_Join" project with join query examples in C#
    - Created `LINQ_Join` project demonstrating LINQ join queries to merge data from `Beer` and `Country` lists.
    - Included `Program.cs` with examples of joins and output formatting.
    - Updated `CsharpCourse.sln` to include the new project and adjusted solution build configurations.

---
+++ introduccion/Variables/LINQ_Join/Program.cs
@@ -0,0 +1,40 @@
+﻿var beers = new List<Beer>()
+{
+    new Beer() { Name = "Corona", Country = "Mexico" },
+    new Beer() { Name = "Delirium", Country = "Belgica" },
+    new Beer() { Name = "Erdinger", Country = "Alemania" }
+};
+
+var countries = new List<Country>()
+{
+    new Country() { Name = "Mexico", Continent = "America" },
+    new Country() { Name = "Belgica", Continent = "Europa" },
+    new Country() { Name = "Alemania", Continent = "Europa" },
+};
+
+var beerWithContinent = from beer in beers
+    join country in countries
+        on beer.Country equals country.Name
+    select new
+    {
+        Name = beer.Name,
+        Country = beer.Country,
+        Continent = country.Continent
+    };
+
+foreach (var beer in beerWithContinent)
+{
+    Console.WriteLine($"{beer.Name} {beer.Country} {beer.Continent}");
+}
+
+public class Beer
+{
+    public string Name { get; set; }
+    public string Country { get; set; }
+}
+
+public class Country
+{
+    public string Name { get; set; }
+    public string Continent { get; set; }
+}
