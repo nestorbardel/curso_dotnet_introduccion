a40d0542bf76cb8b917d30efbfe281e1f12e1f9d
Nestor Bardel <nestorbardel@gmail.com>
2025-07-14 22:19:00 -0300

    feat: Add "BaseDeDatos" project with database connection handling in C#
    - Created `BaseDeDatos` project with basic SQL Server connection management using `System.Data.SqlClient`.
    - Added `DB` class for connection handling (`Connect` and `Close` methods).
    - Included `Program.cs` with example usage of the `DB` class.
    - Updated `CsharpCourse.sln` to include the new project and adjusted solution build configurations.

---
+++ introduccion/Variables/BaseDeDatos/DB.cs
@@ -0,0 +1,29 @@
+﻿namespace BaseDeDatos;
+
+using System.Data.SqlClient;
+
+public class DB
+{
+    private string _connectionstring;
+    protected SqlConnection _connection;
+
+    public DB(string server, string db, string user, string password)
+    {
+        _connectionstring = $"Data Source={server}; Initial Catalog={db};" +
+                            $"User={user};Password={password}";
+    }
+
+    public void Connect()
+    {
+        _connection = new SqlConnection(this._connectionstring);
+        _connection.Open();
+    }
+
+    public void Close()
+    {
+        if (this._connection != null && this._connection.State == System.Data.ConnectionState.Open)
+        {
+            this._connection.Close();
+        }
+    }
+}
---
+++ introduccion/Variables/BaseDeDatos/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace BaseDeDatos;
+
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            DB db = new DB(@"DESKTOP-N9E58US\SQLEXPRESS", "CSharpDB", "", "");
+            db.Connect();
+
+            db.Close();
+
+        }
+        catch (SqlException ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+}
